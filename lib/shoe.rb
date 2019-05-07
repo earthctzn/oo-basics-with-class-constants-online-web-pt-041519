a0717d539b89116f767443a06bbde141d0ca58c4
Caleb Aviles <earthctzn1@gmail.com>
2019-05-07 04:25:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, :size
+  attr_reader :brand,
   
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
-     #if BRANDS.include?(brand)
-      BRANDS << brand
+      BRANDS
   end
   
   
