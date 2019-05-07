1e3963745e964e0838a1e3940ffeb7d09e23b060
Caleb Aviles <earthctzn1@gmail.com>
2019-05-07 04:12:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-      if BRANDS.include? @brand FALSE
-        BRANDS << @brand 
+      if BRANDS.include? brand FALSE
+        BRANDS << brand
       end
     
   end
